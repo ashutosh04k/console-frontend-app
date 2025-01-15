977e1eac5a87b23dc729af7eb026d8d629371833
ashutosh04k <kumarasutosh2014@gmail.com>
2025-01-15 23:25:30 +0530

    admin header

--- src/components/adminview/header.jsx
+++ src/components/adminview/header.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { AlignJustify, LogOut } from "lucide-react";
+import { Button } from "../ui/button";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "@/store/auth-slice/authslice";
+import { message } from "antd";
 
-const header = () => {
+const  AdminHeader =({ setOpen }) => {
+  const dispatch = useDispatch();
+
+  const handleLogout= () => {
+    dispatch(logoutUser());
+    message.success("Logged Out Successfully");
+  }
+
   return (
-    <div>header</div>
-  )
+    <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
+      <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
+        <AlignJustify />
+        <span className="sr-only">Toggle Menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button
+          onClick={handleLogout}
+          className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"
+        >
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+    </header>
+  );
 }
 
-export default header
+export default AdminHeader;
